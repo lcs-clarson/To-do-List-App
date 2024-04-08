89cbc7e3c149af2c855f798abf6f7e457bcd9d77
Charlie Larson <clarson@lcs.on.ca>
2024-04-08 11:19:44 -0400

    Initial Commit

---
+++ To-do List App/To_do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_List_AppApp.swift
+//  To-do List App
+//
+//  Created by Charlie Larson on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct To_do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
